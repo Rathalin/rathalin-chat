7c2a37b53663bf5640c5116fa6d41f533ccd392f
Rathalin <danielflo1997@hotmail.com>
2022-05-16 19:33:54 +0200

    Add simple error page

---
+++ rathalin-chat-svelte/src/routes/__error.svelte
@@ -0,0 +1,18 @@
+<div class="center">
+    <div class="container">
+        <h1>Ye, this page doesn't exist.</h1>
+        <a href="/">Back to main page</a>
+    </div>
+</div>
+
+<style lang="scss">
+    .center {
+        display: flex;
+        justify-content: center;
+    }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+    }
+</style>
