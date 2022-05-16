60f87cbe7eca93a7f0258f3621b3a2168b4a1213
Rathalin <danielflo1997@hotmail.com>
2022-05-16 19:47:44 +0200

    Move border radius to css variables

--- rathalin-chat-svelte/src/lib/component/alert/ErrorComponent.svelte
+++ rathalin-chat-svelte/src/lib/component/alert/ErrorComponent.svelte
@@ -14,7 +14,7 @@
     p {
         flex: 1;
         color: white;
-        border-radius: 2px;
+        border-radius: var(--border-radius-s);
         background-color: var(--alert-danger);
         padding: 1em 1.5em;
         margin-bottom: 1em;
