9a1180d599359a185bc8c3a2da15b8e93b19fdaa
Rhatalin <danielflo1997@hotmail.com>
2022-05-21 16:28:07 +0200

    Fix bug where disconnect wasn't detected in chat

--- rathalin-chat-svelte/src/lib/components/chat/chatroom/ChatroomInputComponent.svelte
+++ rathalin-chat-svelte/src/lib/components/chat/chatroom/ChatroomInputComponent.svelte
@@ -41,7 +41,7 @@ import { iconSize } from "$lib/stores/theme.store";
         }
         joinPending = false;
         joinDone = true;
-        goto("/chat");
+        await goto("/chat");
     }
 </script>
 
--- rathalin-chat-svelte/src/routes/login.svelte
+++ rathalin-chat-svelte/src/routes/login.svelte
@@ -11,25 +11,40 @@
 <script lang="ts">
 	import ChatroomInputComponent from "$lib/components/chat/chatroom/ChatroomInputComponent.svelte";
 	import LoginComponent from "$lib/components/chat/login/LoginComponent.svelte";
-	import { connected, inChatroom, loggedIn } from "$lib/stores/user.store";
+	import { connected, loggedIn } from "$lib/stores/user.store";
 	import { _ } from "$lib/services/i18n/i18n.service";
 	import { chatService } from "$lib/services/chat/chat.service";
 	import { socketIoServerConnection } from "$lib/stores/config.store";
+	import { onDestroy, onMount } from "svelte";
+	import type { Subscription } from "rxjs";
+	const subscriptions: Subscription[] = [];
 
-	chatService.onConnect.subscribe(() => {
-		console.log(`Connected to ${$socketIoServerConnection}`);
-		$connected = true;
+	onMount(() => {
+		subscriptions.push(
+			chatService.onConnect.subscribe(() => {
+				console.log(`Connected to ${$socketIoServerConnection}`);
+				$connected = true;
+			}),
+
+			chatService.onDisconnect.subscribe(() => {
+				console.log(
+					`Disconnected from to ${$socketIoServerConnection}`
+				);
+				$connected = false;
+			}),
+
+			chatService.onConnectError.subscribe((error) => {
+				console.log(
+					`Failed connecting to ${$socketIoServerConnection}`
+				);
+				console.error(error);
+				$connected = false;
+			})
+		);
 	});
 
-	chatService.onDisconnect.subscribe(() => {
-		console.log(`Disconnected from to ${$socketIoServerConnection}`);
-		$connected = false;
-	});
-
-	chatService.onConnectError.subscribe((error) => {
-		console.log(`Failed connecting to ${$socketIoServerConnection}`);
-		console.error(error);
-		$connected = false;
+	onDestroy(() => {
+		subscriptions.forEach((s) => s.unsubscribe());
 	});
 </script>
 
